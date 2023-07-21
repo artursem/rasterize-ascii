c2a5bf5b11543883a8ee29ec0d51348df0da378d
Artur Semeniuk <artur.semeniuk@monterail.com>
2023-07-21 18:37:20 +0200

    chars in dom

--- sketch.js
+++ sketch.js
@@ -7,51 +7,39 @@ img = loadImage('assets/venus-milo.jpg');
 }
 
 function setup() {
-  createCanvas(400, 400);
-  tileSize = 30;
+  noCanvas();
+  tileSize = 1;
   tileCount = width / tileSize
   img.resize(tileCount, tileCount);
   
-  // createLoop({
-    //   duration: 8, 
-    //   framesPerSecond: 60, 
-    //   gif: true,    
-    //   gifFileName: '20230721_rasascii-thumbnail.gif', 
-    //   open: true, 
-    //   download: true
-    // });
-  }
+  background(0);
+
+  let w = width / tileCount;
+  let h = height / tileCount;
+
+  img.loadPixels();
   
-  
-  function draw() {
-    background(220);
+  for (let j=0; j<tileCount; j++) {
+    let row = "";
 
-    let w = width / tileCount;
-    let h = height / tileCount;
+    for  (let i=0; i<tileCount; i++) {
+      const pixelIndex = (i + j * img.width) * 4;
+      const r = img.pixels[pixelIndex + 0];
+      const g = img.pixels[pixelIndex + 1];
+      const b = img.pixels[pixelIndex + 2];
+      const avg = (r+g+b)/3;
 
-    img.loadPixels();
-    
-    for (let i=0; i<tileCount; i++) {
-      for (let j=0; j<tileCount; j++) {
-        const pixelIndex = (i + j * img.width) * 4;
-        const r = img.pixels[pixelIndex + 0];
-        const g = img.pixels[pixelIndex + 1];
-        const b = img.pixels[pixelIndex + 2];
-        const avg = (r+g+b)/3;
-        
-        noStroke();
-        fill(0); 
-        // squares
-        // square(i * w,  j * h, 0.3 * w * avg / 255);
-
-        // chars
-        textSize(w)
-        textAlign(CENTER, CENTER);
-        let length = density.length;
-        const charIndex = Math.floor( map(avg, 0, 255, 0, length));
-        text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5); 
-
+      textSize(w)
+      textAlign(CENTER, CENTER);
+      const length = density.length;
+      const charIndex = Math.floor( map(avg, 0, 255, length, 0));
+      const c = density.charAt(charIndex);
+      if (c === ' ') {
+        row += '&nbsp;';
+      } else {
+        row += c;
       }
-    } 
-
+    }
+    createDiv(row)
+   } 
 }
